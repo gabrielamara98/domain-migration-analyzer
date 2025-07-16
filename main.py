43ea7ab35ed7e9c711d0acbec91150f8653ed1f8
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-07-16 11:49:29 -0300

    Output csv

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import csv
 domain_limitation= str(input("Please set the domain you want to appear in the final list: "))
 file_origin = str(input("Please write the file name with the extension that needs to be read:"))
 
-email_list = []
 email_list_correct = []
 email_list_denied = []
 
 
 with open('domain-migration-analyzer/files/'+file_origin, newline='') as csvfile:
     reader_csv = csv.reader(csvfile)
-
     for elements in reader_csv:
         for email in elements:
-            if email in domain_limitation:
+            email = email.strip()
+            if email.endswith(domain_limitation):
                 email_list_correct.append(email)
             else:
                 email_list_denied.append(email)
-      
-print(email_list_correct)
-print(email_list_denied)
+
+with open('domain-migration-analyzer/files/output_'+ file_origin, 'w', newline='', encoding='utf-8') as readfile:
+    write_csv = csv.writer(readfile)
+    write_csv.writerows([[email] for email in email_list_correct])
+
+
